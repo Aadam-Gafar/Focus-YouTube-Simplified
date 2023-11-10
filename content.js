9367073f6b3ec8ec9a51341e143a5372e58cb31a
Aadam <aadamhgafar@gmail.com>
2023-11-10 14:54:34 +0000

    Fixed bugs
    - UI control now scales
    - sidebar reinstation now accounted for

--- content.js
+++ content.js
@@ -1,10 +1,20 @@
-function removeSidebar() {
+/* YT will try to instate the mini guide in the absence of the extended
+    sidebar. Therefore, we must remove one, wait for the substitution, then
+    remove the other. */
+async function removeSidebar() {
   console.log("Calling 'removeSidebar()'")
   var element = document.querySelector('ytd-app');
-  console.log("--- Element: " + element);
   if (element) {
-    element.removeAttribute('mini-guide-visible');
-    element.removeAttribute('guide-persistent-and-visible');
+    if (element.hasAttribute('mini-guide-visible')) {
+      element.removeAttribute('mini-guide-visible');
+      wait(3000).then(() => element.removeAttribute('guide-persistent-and-visible'));
+    }
+    if (element.hasAttribute('guide-persistent-and-visible')) {
+      element.removeAttribute('guide-persistent-and-visible');
+      wait(3000).then(() => element.removeAttribute('mini-guide-visible'));
+    }
+    //element.removeAttribute('mini-guide-visible');
+    //element.removeAttribute('guide-persistent-and-visible');
   }
 }
 
@@ -16,23 +26,53 @@ function removeRecommendations() {
   }
 }
 
-function resizePlayer() {
+async function resizePlayer() {
   console.log("Resizing player");
-  document.querySelector('video.video-stream.html5-main-video').style.width = '-webkit-fill-available';
-  document.querySelector('video.video-stream.html5-main-video').style.height = 'auto';
+
+  wait(1000);
+  
+  var videoPlayer = document.querySelector('video.video-stream.html5-main-video');
+  var videoControls = document.querySelector('div.ytp-chrome-bottom');
+  var videoBar = document.querySelector('div.ytp-chapter-hover-container');
+
+  if (videoPlayer) {
+    videoPlayer.style.width = '-webkit-fill-available';
+    videoPlayer.style.height = 'auto';
+  }
+
+  if (videoControls) {
+    videoControls.style.width = '-webkit-fill-available';
+    videoControls.style.right = '12px';
+  }
+
+  if (videoBar) {
+    videoBar.style.width = '-webkit-fill-available';
+  }
+  
+
 }
 
-function delay(time) {
+function wait(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
 /* Content is adjusted dynamically after DOM load
-Therefore, we wait one second before removing it. */
-delay(1000).then(() => removeSidebar());
-delay(1000).then(() => removeRecommendations());
-delay(1000).then(() => resizePlayer());
+Therefore, we wait for DOM load before removing items. */
+addEventListener('DOMContentLoaded', removeSidebar());
+wait(500).then(() => removeRecommendations());
+wait(500).then(() => resizePlayer());
+//removeRecommendations();
+//resizePlayer();
 
+/* When screen size thresholds are reached, YT tries to reinstate the sidebar
+Also, the video player needs manual resizing due to our other changes. */
 window.addEventListener("resize", (event) => {
   removeSidebar();
   resizePlayer();
+});
+
+// When the home button is clicked, YT tries to reinstate the sidebar
+document.getElementById('start').addEventListener('click', (event) => {
+  console.log("Button event");
+  removeSidebar();
 });
