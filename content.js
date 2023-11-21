162b0a04c89d87759add5fa8a77ac0fa95e3c6c9
Aadam <aadamhgafar@gmail.com>
2023-11-21 14:50:23 +0000

    Adaptable nav bar

--- content.js
+++ content.js
@@ -3,15 +3,16 @@
 const targetNode = document.getElementById("content"); // Observer target
 const config = { attributes: true, childList: true, subtree: true }; // Observer settings
 const callback = (mutationList, observer) => { // Function to execute when observer detects mutations in target
+  setHome();
   removeRecommendations();
   cleanPills();
 };
 
-// #endregion
-
 // Create observer instance and begin observing for mutations
 const observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
+
+// #endregion
 
 // #region FUNCTIONS
 
@@ -23,23 +24,38 @@ function removeRecommendations() {
   }
 }
 
-// Removes 'Shorts' pill on search page
-function cleanNavbar() {
-  var element = document.getElementById('end');
-  console.log("Cleaning navbar run" + element);
-  if (element) {
-    element.style.display = 'none';
-    element.remove();
-  }
-}
-
-function cleanPills() { // Removes 'Shorts' pill from Search page
+function cleanPills() { // Removes 'Shorts' pill from search page
   var element = document.querySelector('[title="Shorts"]');
   if (element) {
     var parent = element.parentElement;
     if (parent) {
       parent.remove();
     }
+  }
+}
+
+function setHome() { // Centres the search bar on the home page
+  var currentURL = (window.location.href).slice(0, -1); // Ignore last character (e.g. '/')
+  var slashCount = (currentURL.match(/\//g) || []).length;
+  var navElement = document.getElementById('masthead-container');
+
+  var element = document.querySelector('.style-scope.ytd-app');
+  if (element && element.hasAttribute('hidden')) {
+    console.log("Hidden");
+  }
+
+  if (currentURL && slashCount < 3) {
+    if (navElement) {
+      console.log("Restyling nav bar!");
+      navElement.style.height = '-webkit-fill-available';
+      navElement.style.display = 'flex';
+      navElement.style.flexDirection = 'column';
+      navElement.style.justifyContent = 'center';
+    }
+  } else {
+    console.log("Returning to normal...");
+    navElement.style.height = 'auto !important';
+    navElement.style.setProperty('height', 'auto', 'important');
   }
 }
 
